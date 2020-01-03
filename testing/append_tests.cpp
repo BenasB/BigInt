c3c175d38a32fa2b6a00496fbe36ad060fd28fd7
Benas Budrys <29711974+BenasB@users.noreply.github.com>
2020-01-03 16:34:02 +0200

    Added another test case

--- testing/append_tests.cpp
+++ testing/append_tests.cpp
@@ -3,7 +3,7 @@
 #include "catch.hpp"
 #include "../BigInt.h"
 
-TEST_CASE ( "Appending", "[append]" )
+TEST_CASE ( "AppendingSingleDigitAsInt", "[append]" )
 {
     BigInt bigInt;
     bigInt.appendDigit(1);
@@ -11,6 +11,18 @@ TEST_CASE ( "Appending", "[append]" )
     bigInt.appendDigit(3);
 
     std::string expectedResult = "123";
+
+    REQUIRE(expectedResult == bigInt.getDigitsString());
+}
+
+TEST_CASE ( "AppendingMultipleDigitsAsInt", "[append]" )
+{
+    BigInt bigInt;
+    bigInt.appendDigits(12);
+    bigInt.appendDigits(47);
+    bigInt.appendDigits(25);
+
+    std::string expectedResult = "124725";
 
     REQUIRE(expectedResult == bigInt.getDigitsString());
 }
